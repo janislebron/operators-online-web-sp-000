359bced5f680540f1cdf11f91f551c099c10eabd
Janis Lebron <janislebron@gmail.com>
2020-05-03 22:33:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+	if speed > 60
+		returns true
+	elsif speed < 40
+		returns true
+	else speed > 60 || speed < 40
+		returns false
+	end	
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60 || speed < 40 ? true : false
 end
-	
-
-
